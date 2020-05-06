0b83bb486829eff38f5997a36dd3a673e42e42e6
Rockwell <>
2020-05-06 09:08:16 +0700

    multithreading for login

--- ftp_server.py
+++ ftp_server.py
@@ -1,52 +1,44 @@
 # Connection Establishment and FTP Command Learning Resource
 # http://slacksite.com/other/ftp.html
 # https://tools.ietf.org/html/rfc959
-
 import socket
+import threading
 
 socket_cmd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#socket_data = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 portCMD = 21
-#portDATA = 20
-
 socket_cmd.bind(('', portCMD))
-#socket_data.bind(('', portDATA))
-
 socket_cmd.listen(100)
 
-#def send_data(data):
-#    data += "\r\n"
-#    data = data.encode()
-#    data_conn.send()
-#
-#def recv_data(data):
-#    data_conn.recv()
+def login(conn):
+        send_cmd("220 FTPproject v1.0",conn)
+        username = recv_cmd(conn)
+        send_cmd("331 Please specify the password.",conn)
+        password = recv_cmd(conn)
+        send_cmd("230 " + username + " Logged in.",conn)
 
-def send_cmd(data):
+
+
+def conn_threading(conn):
+    login(conn)
+
+def send_cmd(data,conn):
     data += "\r\n"
     data = data.encode()
-    cmd_conn.send(data)
+    conn.send(data)
     print(data)
 
-def recv_cmd():
-    data = cmd_conn.recv(1024)
+def recv_cmd(conn):
+    data = conn.recv(1024)
     print(data)
     return data.decode()
 
 print("---> Server Socket Listening")
 
-cmd_conn, cmd_addr = socket_cmd.accept()
-
-print("--> Connected from", cmd_addr)
-
-#port_data = cmd_addr[1] + 1
-
-send_cmd("220 FTPproject v1.0")
-username = recv_cmd()
-send_cmd("331 Please specify the password.")
-password = recv_cmd()
-send_cmd("230 " + username + " Logged in.")
+while True:
+    cmd_conn, cmd_addr = socket_cmd.accept()
+    print("---> Connected from", cmd_addr)
+    connThread = threading.Thread(target=conn_threading, args=(cmd_conn,))
+    connThread.start()
 
 socket_cmd.close()
 socket_data.close()
